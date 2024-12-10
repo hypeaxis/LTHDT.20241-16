fbd4620c93d3c295b24056e6d775ab3406fbdb3b
Amyl11 <vumanhcvp@gmail.com>
2024-12-10 22:50:38 +0700

    khởi tạo, xây dựng phương thức tính toán trọng lực, phản lực, lực ma sát(4 trường hợp)

--- sourcecode/src/force/AppliedForce.java
+++ sourcecode/src/force/AppliedForce.java
@@ -2,4 +2,11 @@ package force;
 
 public class AppliedForce extends Force{
 
+	public AppliedForce(float value, String direction) {
+		super();
+		this.setDirection(direction);
+		this.setValue(value);
+	}
+	
+	
 }
--- sourcecode/src/force/Gravity.java
+++ sourcecode/src/force/Gravity.java
@@ -1,5 +1,15 @@
 package force;
 
+import mainObject.MainObject;
+
 public class Gravity extends Force{
 	
+	public Gravity() {
+		super();
+	}
+
+	public void calculateGravity(MainObject obj) {
+		this.setDirection("down");
+		this.setValue(10 * obj.getMass());
+	}
 }
